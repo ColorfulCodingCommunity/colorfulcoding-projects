3472639d1d71b3ee83a868c8e993a70c8a31cd1a
Dragos Silion <silidragos@gmail.com>
2020-06-11 20:30:41 +0300

    PUN2 setup + Photon Voice

---
+++ TCG VR/Networking/NetworkVoiceManager.cs
@@ -0,0 +1,64 @@
+﻿using Photon.Pun;
+using Photon.Voice.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VoiceConnection))]
+public class NetworkVoiceManager : MonoBehaviour
+{
+    public static event Action<bool> OnMuteSwitch;
+
+    public static bool isMute = false;
+
+    public Transform remoteVoiceParent;
+    public AudioClip muteUnmuteSound;
+
+    private VoiceConnection voiceConnection;
+    private GeneralSoundSystemBehavior generalSoundSystemBehavior;
+    
+    void Awake()
+    {
+        generalSoundSystemBehavior = GeneralSoundSystemBehavior.Instance;
+        voiceConnection = GetComponent<VoiceConnection>();
+
+        voiceConnection.Client.NickName = GeneralSettings.myNickname;
+    }
+
+    private void OnEnable()
+    {
+        voiceConnection.SpeakerLinked += this.OnSpeakerCreated;
+        ControlsFacade.OnMuteUnmute += this.SwitchMute;
+    }
+
+    private void OnDisable()
+    {
+        voiceConnection.SpeakerLinked -= this.OnSpeakerCreated;
+        ControlsFacade.OnMuteUnmute -= this.SwitchMute;
+    }
+
+    private void OnSpeakerCreated(Speaker speaker)
+    {
+        speaker.transform.SetParent(this.remoteVoiceParent);
+        speaker.OnRemoteVoiceRemoveAction += OnRemoteVoiceRemove;
+    }
+
+    private void OnRemoteVoiceRemove(Speaker speaker)
+    {
+        if(speaker != null)
+        {
+            Destroy(speaker.gameObject);
+        }
+    }
+
+    private void SwitchMute()
+    {
+        isMute = !isMute;
+
+        GetComponent<Recorder>().IsRecording = !isMute;
+        generalSoundSystemBehavior.PlaySound(muteUnmuteSound);
+        OnMuteSwitch?.Invoke(isMute);
+
+    }
+}
