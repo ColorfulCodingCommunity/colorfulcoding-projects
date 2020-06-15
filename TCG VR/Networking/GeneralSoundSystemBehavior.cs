e5361569731b63b9e69a7d824c04bef14dc7cfd9
Dragos Silion <silidragos@gmail.com>
2020-06-15 09:52:40 +0300

    General Sound System

---
+++ TCG VR/Networking/GeneralSoundSystemBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralSoundSystemBehavior : MonoBehaviour
+{
+    #region SINGLETON
+    private static GeneralSoundSystemBehavior _instance;
+    public static GeneralSoundSystemBehavior Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GeneralSoundSystemBehavior>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("No GeneralSoundSystem found in scene!");
+                }
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+    void Update()
+    {
+        transform.position = Camera.main.transform.position;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        var audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
